2fec0c91b57dc2419173cef775a1baf690569451
LeoG123321 <157080843+LeoG123321@users.noreply.github.com>
2024-03-07 17:14:32 -0600

    Add project files.

---
+++ i_get_the_point/i_get_the_point.cpp
@@ -0,0 +1,14 @@
+/*
+    Leonardo Gonzalez   3/7/2024
+
+    Week 8              I Get the Point!
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+}
+
